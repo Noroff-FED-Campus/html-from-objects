d11edb81435d952c7243c5a21c3d6daa27696bce
Monde Sineke <nsineke@gmail.com>
2023-01-27 00:14:11 +0100

    🎉 init(*): Init Vite

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hi! This content come from javascript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+    </p>
+  </div>
+`;
